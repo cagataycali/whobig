526ff64454ccdb86d1dac2a7ccc5a767f9b774c7
cagataycali <calicagatay@icloud.com>
2016-11-26 10:34:53 -0800

    ✨  Bigger is better

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const E = require('3x3c');
 const colors = require('colors');
 
+console.log("Bigger is better :))");
 E(`du -hsx * | sort -rh | head -10`)
   .then((output) => {
     colors.green(output)
