8ed01b4ef96d8bb2a9d89b00b912c534273f4dd0
cagataycali <calicagatay@icloud.com>
2016-11-26 11:21:42 -0800

    ✨  Mac os x support

--- README.md
+++ README.md
@@ -2,6 +2,4 @@
 
 * Shows which files is bigger in directory.
 
-* Works in only linux.
-
 * Simple is the best.
--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 const E = require('3x3c');
 const colors = require('colors');
 
-console.log("Bigger is better :))");
-E(`du -hsx * | sort -rh | head -10`)
+var cmd = /^dar/.test(process.platform) ? 'du -ax * | sort -rn | head -10' : 'du -hsx * | sort -rh | head -10';
+console.log(colors.rainbow('Bigger is better :))'));
+E(cmd)
   .then((output) => {
     console.log(colors.green(output));
   })
